5a8dba91d8a9f95cf619905a86e7b888b75b726a
lukastars <bonger2014@gmail.com>
2020-06-17 12:53:06 +0200

    first commit

---
+++ blog/index.php
@@ -0,0 +1,372 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+  <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-164606205-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+
+    function gtag() {
+      dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'UA-164606205-1');
+  </script>
+  <title>OpenTabs Blog</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="Description"
+    content="OpenTabs Blog, OpenTabs is a free web browser extension that allows you to passively raise funds to reduce poverty and climate change">
+  <link rel="shortcut icon" href="../images/logo.png" />
+
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:300, 400,700" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@600&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@700&display=swap" rel="stylesheet">
+
+  <link rel="stylesheet" href="css/bootstrap.css">
+  <link rel="stylesheet" href="css/animate.css">
+  <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+  <link rel="stylesheet" href="fonts/fontawesome/css/all.css">
+
+
+  <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+  <link rel="stylesheet" href="fonts/fontawesome/css/all.css.css">
+  <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+  <link rel="stylesheet" href="https://unpkg.com/tippy.js@6/animations/scale.css" />
+
+
+
+  <!-- Theme Style -->
+  <link rel="stylesheet" href="css/style.css">
+</head>
+
+<body>
+
+
+
+  <header role="banner">
+
+
+    <div class="container logo-wrap">
+      <div class="row pt-5">
+        <div class="col-12 text-center">
+          <a class="absolute-toggle d-block d-md-none" data-toggle="collapse" href="#navbarMenu" role="button"
+            aria-expanded="false" aria-controls="navbarMenu"><span class="burger-lines"></span></a>
+          <h1 class="site-logo"><a href="/blog">OpenTabs Blog</a></h1>
+        </div>
+      </div>
+    </div>
+    <nav class="navbar navbar-expand-md  navbar-light bg-light">
+      <div class="container">
+
+
+        <div class="collapse navbar-collapse" id="navbarMenu">
+          <ul class="navbar-nav mx-auto">
+
+            <li class="nav-item">
+              <a class="nav-link " href="/blog">Blog</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link " href="../">Home</a>
+            </li>
+
+
+            <li class="nav-item">
+              <a class="nav-link" href="../financials">Financials</a>
+            </li>
+          </ul>
+
+        </div>
+      </div </nav> </header> <!-- END header -->
+
+      <section class="site-section pt-5">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+
+              <div class="owl-carousel owl-theme home-slider">
+                <div>
+                  <a href="en-que-consisten-los-microcreditos.html" class="a-block d-flex align-items-center height-lg"
+                    style="background-image: url('blog images/article_1thump.jpg'); ">
+                    <div class="text half-to-full">
+                      <div class="post-meta">
+                        <span class="mr-2">May 3, 2020 </span>
+                      </div>
+                      <h3>¿En qué consisten los microcréditos?</h3>
+                      <p>Parece mentira que hoy en día sigue habiendo más de 600 millones de personas (8% de la
+                        población
+                        mundial) viviendo en pobreza extrema (ganando menos de $1.9 al día), y muchos mas viviendo en
+                        pobreza
+                        relativa. Pero la buena noticia es que hemos reducido esta estadística drásticamente (con 2.000
+                        millones de personas viviendo en pobreza extrema en los 80).</p>
+
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="How-can-you-help-OpenTabs.html" class="a-block d-flex align-items-center height-lg"
+                    style="background-image: url('blog images/help-ot.jpg'); ">
+                    <div class="text half-to-full">
+                      <div class="post-meta">
+                        <span class="mr-2">May 6, 2020 </span>
+                      </div>
+                      <h3>How can you help OpenTabs?</h3>
+                      <p>OpenTabs is a non-profit that primarily relies on the goodwill of others. Be it our users,
+                        sponsors, donors, or key partners, there’s a huge community helping us make the world a
+                        better place. If you would like to learn more about how you can help, here are three very
+                        important ways</p>
+
+                    </div>
+                  </a>
+                </div>
+                <div>
+                  <a href="How-to-Solve-Poverty-&-Why-is-it-Our-Problem.html"
+                    class="a-block d-flex align-items-center height-lg"
+                    style="background-image: url('blog images/end-poverty.jpg'); ">
+                    <div class="text half-to-full">
+                      <div class="post-meta">
+                        <span class="mr-2">May 10, 2020 </span>
+                      </div>
+                      <h3>How to Solve Poverty &amp; Why is it “Our” Problem?</h3>
+                      <p>In 2015, the United Nations set up the 17 global Sustainable Development Goals (SDG), with
+                        the first goal to “end poverty in all its forms everywhere”. About 600 million people (8% of
+                        the world population) live in extreme poverty, and many people often think of poverty as
+                        “their” problem. But the reality is far from it. For example, global pandemics (such as Ebola
+                        or COVID-19) are more likely to spur and spread due to a lack of basic health services in
+                        parts of the world, and young men who are desperate for food are also more likely to join
+                        guerrillas and terrorist groups.</p>
+
+
+                    </div>
+                  </a>
+                </div>
+              </div>
+
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-6 col-lg-4">
+              <a href="How-to-install-Ope-Tabs-on-Safari.html" class="a-block d-flex align-items-center height-md"
+                style="background-image: url(images/safari-thumpnail.png); ">
+                <div class="text">
+                  <div class="post-meta">
+                    <span class="mr-2">June 15, 2020 </span>
+                  </div>
+                  <h3>How to install OpenTabs on Safari?</h3>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+              <a href="How-to-Solve-Poverty-&-Why-is-it-Our-Problem.html"
+                class="a-block d-flex align-items-center height-md"
+                style="background-image: url('blog images/end-poverty.jpg'); ">
+                <div class="text">
+                  <div class="post-meta">
+                    <span class="mr-2">May 10, 2020 </span>
+                  </div>
+                  <h3>How to Solve Poverty &amp; Why is it “Our” Problem?</h3>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-6 col-lg-4">
+              <a href="The-Five-Global-Risks-We-Should-Worry-About.html"
+                class="a-block d-flex align-items-center height-md"
+                style="background-image: url('blog images/global-risks.jpg'); ">
+                <div class="text">
+                  <div class="post-meta">
+                    <span class="mr-2">May 8, 2020 </span>
+                  </div>
+                  <h3>The Five Global Risks We Should Worry About
+                  </h3>
+                </div>
+              </a>
+            </div>
+
+            <div class="col-md-6 col-lg-6">
+              <a href="How-can-you-help-OpenTabs.html" class="a-block d-flex align-items-center height-md"
+                style="background-image: url('blog images/help-ot.jpg');  ">
+                <div class="text">
+                  <div class="post-meta">
+                    <span class="mr-2">May 6, 2020 </span>
+                  </div>
+                  <h3>How can you help OpenTabs? </h3>
+                </div>
+              </a>
+            </div>
+
+
+            <div class="col-md-6 col-lg-6">
+              <a href="en-que-consisten-los-microcreditos.html" class="a-block d-flex align-items-center height-md"
+                style="background-image: url('blog images/article_1thump.jpg'); ">
+                <div class="text">
+                  <div class="post-meta">
+                    <span class="mr-2">May 3, 2020 </span>
+                  </div>
+                  <h3>¿En qué consisten los microcréditos?</h3>
+                </div>
+              </a>
+            </div>
+          </div>
+        </div>
+
+
+      </section>
+      <!-- END section -->
+
+
+
+      <footer class="page-footer font-small indigo" style="
+      font-family: Nunito, sans-serif;
+      
+  ">
+
+        <!-- Footer Links -->
+        <div class="container">
+
+          <!-- Grid row-->
+          <div class="row text-center d-flex justify-content-center pt-5 mb-3 mt">
+
+            <!-- Grid column -->
+            <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a href="https://forms.gle/TqrAMAf5Pc6U8WK38" target="_blank">Feedback</a>
+              </h6>
+            </div>
+            <!-- Grid column -->
+
+            <div class="col-md-1 mb-3">
+              <h6 class="font-weight-bold">
+                <a href="../faq/index.html">FAQ</a>
+              </h6>
+            </div>
+
+            <!-- Grid column -->
+            <div class="col-md-1 mb-3">
+              <h6 class="font-weight-bold">
+                <a href="../blog">Blog</a>
+              </h6>
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+
+            <!---   <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a href="#!" class="soon" data-tippy-content="Coming Soon!" target="">Search</a>
+              </h6>
+            </div>-->
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a href="/">Home</a>
+              </h6>
+            </div>
+            <!-- Grid column -->
+            <!--  <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a href="#!" class="soon" data-tippy-content="Coming Soon!">Flights</a>
+              </h6>
+            </div> -->
+            <!-- Grid column -->
+            <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a href="../financials">Financials</a>
+              </h6>
+            </div>
+            <!-- Grid column -->
+            <div class="col-md-1 mb-3">
+              <h6 class=" font-weight-bold">
+                <a target="_blank" href="https://forms.gle/Ymj4iTeauzPLdEvs6">Contact</a>
+              </h6>
+            </div>
+            <!-- Grid column -->
+
+          </div>
+          <!-- Grid row-->
+          <hr class="rgba-white-light" style="margin: 0 15%; padding-bottom: 20px;">
+
+          <!-- Grid row-->
+
+          <!-- Grid row-->
+          <hr class="clearfix d-md-none rgba-white-light" style="margin: 10% 15% 5%;">
+
+          <!-- Grid row-->
+          <div class="row  ">
+            <!-- Grid column -->
+            <div class="col-md-12 d-flex justify-content-center" style="
+						height: 23px;
+					">
+
+              <div class="mb-5 flex-center">
+
+                <!--Instagram-->
+                <a href="https://www.instagram.com/open_tabs/" class="ins-ic">
+                  <i class="fab fa-instagram fa-lg white-text mr-4"> </i>
+                </a>
+
+                <!--Linkedin -->
+                <a href="https://linkedin.com/company/opentabs-org" class="li-ic">
+                  <i class="fab fa-linkedin-in fa-lg white-text mr-4"> </i>
+                </a>
+
+
+                <!-- youtube -->
+                <a href="https://www.youtube.com/channel/UC7Ggi0aAxmxHtoMqZ6BckBg" class="tw-ic">
+                  <i class="fab fa-youtube fa-lg white-text mr-4"> </i>
+                </a>
+
+              </div>
+
+            </div>
+            <!-- Grid column -->
+
+          </div>
+          <!-- Grid row-->
+
+        </div>
+        <!-- Footer Links -->
+
+        <!-- Copyright -->
+        <div class="footer-copyright text-center py-3 " style="font-family:  Nunito , sans-serif; font-weight: 
+				600; color: #0080ff; ">
+          <p style="
+					margin-right: 27px;
+					margin-bottom: 0px;
+				"> © 2020
+            OpenTabs</p>
+
+        </div>
+        <!-- Copyright -->
+
+      </footer>
+      <!-- END footer -->
+
+      <!-- loader -->
+      <div id="loader" class="show fullscreen"><svg class="circular" width="48px" height="48px">
+          <circle class="path-bg" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke="#eeeeee" />
+          <circle class="path" cx="24" cy="24" r="22" fill="none" stroke-width="4" stroke-miterlimit="10"
+            stroke="#f4b214" /></svg></div>
+
+      <script src="https://unpkg.com/@popperjs/core@2"></script>
+      <script src="https://unpkg.com/tippy.js@6"></script>
+      <script src="js/jquery-3.2.1.min.js"></script>
+      <script src="js/jquery-migrate-3.0.0.js"></script>
+      <script src="js/popper.min.js"></script>
+      <script src="js/bootstrap.min.js"></script>
+      <script src="js/owl.carousel.min.js"></script>
+      <script src="js/jquery.waypoints.min.js"></script>
+      <script src="js/jquery.stellar.min.js"></script>
+
+
+      <script src="js/main.js"></script>
+</body>
+
+</html>
