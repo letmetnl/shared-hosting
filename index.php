5cab2d4761b096bad562ceb8fd3714cd0a8468ae
lukastars <bonger2014@gmail.com>
2020-07-11 00:39:35 +0200

    commit

--- index.php
+++ index.php
@@ -431,7 +431,9 @@ font-family: Nunito;background-color: #fff;
 						</div>
 					</div>
 					<div class="col-lg-2 col-md-2 ">
-						<div class="d-flex justify-content-center">
+						<div class="d-flex justify-content-center0" style="
+												margin-left: 20px;
+											">
 							<a href="http://www.jacobs-startup.com/" target="_blank"><img
 									class="hvr-grow d-flex align-items-center" src="images/Jacobs.png" alt=""
 									style="height: 92px;margin-bottom: 10px;"></a>
